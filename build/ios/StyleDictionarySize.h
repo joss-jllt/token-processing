8bf56e637f673b2b1163780d168f8daab95f0101
Josselin Cuette <josselin.cuette@am.jll.com>
2021-11-10 16:33:56 -0500

    no message

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Wed, 10 Nov 2021 21:24:56 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
