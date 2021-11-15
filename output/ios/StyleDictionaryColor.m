7c5d05619c9849eafa8d4c8c544ad875a2424eb2
joss-jllt <joss-jllt@users.noreply.github.com>
2021-11-15 20:13:00 +0000

    Update platform deliverables

--- output/ios/StyleDictionaryColor.m
+++ output/ios/StyleDictionaryColor.m
@@ -3,7 +3,7 @@
 // StyleDictionaryColor.m
 //
 // Do not edit directly
-// Generated on Mon, 15 Nov 2021 20:07:28 GMT
+// Generated on Mon, 15 Nov 2021 20:12:59 GMT
 //
 
 #import "StyleDictionaryColor.h"
--- output/ios/StyleDictionarySize.m
+++ output/ios/StyleDictionarySize.m
@@ -3,7 +3,7 @@
 // StyleDictionarySize.m
 //
 // Do not edit directly
-// Generated on Mon, 15 Nov 2021 20:07:28 GMT
+// Generated on Mon, 15 Nov 2021 20:12:59 GMT
 //
 
 #import "StyleDictionarySize.h"
