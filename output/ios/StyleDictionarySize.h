116d782a427b429a6c6531a5bceac45fc79a7a6f
joss-jllt <joss-jllt@users.noreply.github.com>
2021-11-15 20:36:37 +0000

    Update platform deliverables

--- output/ios/StyleDictionarySize.h
+++ output/ios/StyleDictionarySize.h
@@ -2,7 +2,7 @@
 // StyleDictionarySize.h
 //
 // Do not edit directly
-// Generated on Mon, 15 Nov 2021 20:34:57 GMT
+// Generated on Mon, 15 Nov 2021 20:36:37 GMT
 //
 
 #import <Foundation/Foundation.h>
--- output/ios/StyleDictionarySize.m
+++ output/ios/StyleDictionarySize.m
@@ -3,7 +3,7 @@
 // StyleDictionarySize.m
 //
 // Do not edit directly
-// Generated on Mon, 15 Nov 2021 20:34:57 GMT
+// Generated on Mon, 15 Nov 2021 20:36:37 GMT
 //
 
 #import "StyleDictionarySize.h"
